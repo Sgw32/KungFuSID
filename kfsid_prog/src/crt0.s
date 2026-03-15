71f4f4035a30a1a8c90e248df0bb1d367b7c0c32
Sgw32 <sgw32nc@gmail.com>
2026-03-15 23:44:46 +0300

    Adjust KFSID Prog appearance and make it running. Not tested on real hardware

--- kfsid_prog/src/crt0.s
+++ kfsid_prog/src/crt0.s
@@ -4,22 +4,19 @@
 ;
 ; This must be the *first* file on the linker command line
 ;
-
+    .export         __STARTUP__ : absolute = 1      ; Override c64.lib's crt0.s
+    
     .import        initlib, donelib, callirq
     .import        zerobss
     .import        callmain
     .import        RESTOR, BSOUT, CLRCH
     .import        __INTERRUPTOR_COUNT__
-    .import        __RAM_START__, __RAM_SIZE__    ; Linker generated
+    .import        __MAIN_START__, __MAIN_SIZE__    ; Linker generated
 
     .importzp       sp, sreg, regsave
     .importzp       ptr1, ptr2, ptr3, ptr4
     .importzp       tmp1, tmp2, tmp3, tmp4
     .importzp       regbank
-
-    zpspace = 26
-
-IRQVec              := $0314
 
 ; ------------------------------------------------------------------------
 ; Place the startup code in a special segment.
@@ -38,12 +35,6 @@ Head:   .word   @Next
 ; ------------------------------------------------------------------------
 ; Actual code
 
-        ldx #zpspace-1
-L1:     lda sp,x
-        sta zpsave,x    ; Save the zero page locations we need
-        dex
-        bpl L1
-
         lda #$36        ; Hide BASIC/CART
         sta $01
 
@@ -57,13 +48,10 @@ L1:     lda sp,x
         ; Clear the BSS data
         jsr zerobss
 
-        tsx
-        stx spsave      ; Save the system stack ptr
-
         ; Set argument stack ptr
-        lda #<(__RAM_START__ + __RAM_SIZE__)
+        lda #<(__MAIN_START__ + __MAIN_SIZE__)
         sta sp
-        lda #>(__RAM_START__ + __RAM_SIZE__)
+        lda #>(__MAIN_START__ + __MAIN_SIZE__)
         sta sp+1
 
         ; Call module constructors
@@ -71,30 +59,3 @@ L1:     lda sp,x
 
         ; Call main - will never return
         jmp callmain
-
-; ------------------------------------------------------------------------
-; The IRQ vector jumps here, if condes routines are defined with type 2.
-
-IRQStub:
-        cld             ; Just to be sure
-        jsr callirq     ; Call the functions
-        jmp IRQInd      ; Jump to the saved IRQ vector
-
-; ------------------------------------------------------------------------
-.data
-
-IRQInd:
-        jmp $0000
-
-
-; ------------------------------------------------------------------------
-.segment "ZPSAVE"
-
-zpsave:
-        .res zpspace
-
-; ------------------------------------------------------------------------
-.bss
-
-spsave:
-        .res 1
